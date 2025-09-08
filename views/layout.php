bafcf834ae2c609faf292fb13d7e1615a348959d
almalikzakwan <almalikzakwan@gmail.com>
2025-09-08 11:12:05 +0800

    update layout info

--- views/layout.php
+++ views/layout.php
@@ -23,7 +23,7 @@
 
     <footer class="bg-gray-800 text-white p-4">
         <div class="container mx-auto text-center">
-            <p>&copy; 2024 Maps Service Engine. All rights reserved.</p>
+            <p>amzhnpi &copy; <?= date('Y') ?> Maps Service Engine. All rights reserved.</p>
         </div>
     </footer>
 </body>
