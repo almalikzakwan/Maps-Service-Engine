dfc05c3a8a0f869b8b8476c94732359de9b1233a
almalikzakwan <almalikzakwan@gmail.com>
2025-09-08 10:26:52 +0800

    config helper for maps

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'tile_cache_dir' => '/tmp/tile_cache/',
+    'tile_source' => 'https://tile.openstreetmap.org',
+    'max_zoom' => 18,
+    'min_zoom' => 0,
+    'enable_cors' => true,
+    'cache_tiles' => true,
+    'cache_ttl' => 3600, // 1 hour
+];
